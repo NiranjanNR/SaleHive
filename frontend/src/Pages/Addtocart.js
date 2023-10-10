7f31e857930c92879b8240b2ca80d404e9211622
NiranjanNR <kidkool8@gmail.com>
2023-10-10 23:50:32 +0530

    fix merge error

---
+++ frontend/src/Pages/Addtocart.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import NavBar from '../components/NavBar/NavBar';
+import Footer from '../components/Footer';
+import Search from '../components/Search';
+import Items from '../components/Items/Items';
+
+const Addtocart = () => {
+
+    return (
+        <div className=''>
+            <div className=''>
+                <NavBar />
+
+            </div>
+            {/* <Footer /> */}
+        </div>
+    )
+}
+
+export default Addtocart
