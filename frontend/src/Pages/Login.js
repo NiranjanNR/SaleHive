6c9d58478239c731535598b2fafef6fe97c46589
NiranjanNR <kidkool8@gmail.com>
2023-10-11 14:53:47 +0530

    Added invalid username and password

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -18,32 +18,39 @@ const Home = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
+    const [invalid, setInvalid] = useState(false);
 
-    async function onLogin(){
-        const {data, status} = await axios.post('/api/login', {
-            "email": email,
-            "password": password,
-        });
+    async function onLogin() {
+        try {
+            const { data, status } = await axios.post('/api/login', {
+                "email": email,
+                "password": password,
+            });
 
-        if(status == 200){
-            navigate('/')
+            if (status === 200) {
+                navigate('/');
+            } else {
+                setInvalid(true); // Set invalid to true when login fails
+            }
+        } catch (error) {
+            console.error(error);
+            setInvalid(true); // Set invalid to true when there is an error
         }
-        console.log(data);
     }
 
     async function checkSessionID() {
-        try{
-          const {data, status} = await axios.get('/api/user');
-          if(status == 200){
-            navigate('/')
-          }
-        } catch(e){
-          console.log(e)
+        try {
+            const { data, status } = await axios.get('/api/user');
+            if (status == 200) {
+                navigate('/')
+            }
+        } catch (e) {
+            console.log(e)
         }
-      }
-      useEffect(() => {
+    }
+    useEffect(() => {
         checkSessionID()
-      }, []);
+    }, []);
 
     return (
         <div className='h-[85vh]'>
@@ -55,13 +62,18 @@ const Home = () => {
                     <div>
                         <div className='font-semibold text-[30px] flex justify-center'>Welcome Back</div>
                         <div className="w-[300px] mt-8">
-                            <Input variant="outlined" color='brown' size='lg' className='py-6' label='Email address' onChange={(e) => {setEmail(e.target.value)}} />
+                            <Input variant="outlined" color='brown' size='lg' className='py-6' label='Email address' onChange={(e) => { setEmail(e.target.value) }} />
                         </div>
                         <div className="w-[300px] mt-8">
-                            <Input variant="outlined" color='brown' size='lg' className='py-6' label='Password' type="password"  onChange={(e) => {setPassword(e.target.value)}}/>
+                            <Input variant="outlined" color='brown' size='lg' className='py-6' label='Password' type="password" onChange={(e) => { setPassword(e.target.value) }} />
                         </div>
                         <div className="w-[300px] mt-6">
                             <div onClick={() => onLogin()} className='text-white bg-brown-600 hover:bg-brown-700 rounded-sm h-[50px] text-[16px] flex justify-center items-center'>Continue</div>
+                        </div>
+                        <div className="w-[300px] mt-6 text-[14px] flex justify-center">
+                            {invalid ? (
+                                <span className='text-red-600'>Invalid email or password</span>
+                            ) : null}
                         </div>
                         <div className="w-[300px] mt-6 text-[14px] flex justify-center">
                             Dont have an account? <span className='ml-2 text-brown-600 hover:text-brown-800'> Sign up</span>
